5eff1cd27d77303b2cf69da5a5713e57c8250d9b
brendan <brendan@crunchbang.(none)>
2016-04-03 14:31:22 -0400

    ajout classe socket

--- common/include/typos.h
+++ common/include/typos.h
@@ -9,4 +9,10 @@
 typedef std::map<std::string, std::string>  ss_map;
 typedef std::list<std::string>              s_list;
 
+namespace Constants
+{
+	static const int DEFAULT_READ_SIZE = 1024;
+	static const int DEFAULT_WRITE_SIZE = 1024;
+}
+
 #endif /* !TYPOS_H_ */
